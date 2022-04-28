649b73fe960a4ff8c5337fbfddb4d048a941ae58
adrian-lucas <donplucas@gmail.com>
2022-04-28 00:07:29 -0400

    registro de evento conectada a BD remota

--- app/src/main/java/com/cdp/agenda/NuevoActivity.java
+++ app/src/main/java/com/cdp/agenda/NuevoActivity.java
@@ -1,13 +1,12 @@
 package com.cdp.agenda;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -16,9 +15,21 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cdp.agenda.db.DbContactos;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NuevoActivity extends AppCompatActivity {
 
@@ -27,6 +38,7 @@ public class NuevoActivity extends AppCompatActivity {
     Button btnGuarda, bFecha, bHora;
     Activity actividad;
     private int dia, mes, anio, hora, minutos;
+    String titulo,time,fecha,descripcion,direccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,21 +60,31 @@ public class NuevoActivity extends AppCompatActivity {
         txtDescripcion = findViewById(R.id.txtDescripcion);
         btnGuarda = findViewById(R.id.btnGuarda);
         actividad=this;
+
+
+
         btnGuarda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                titulo= txtTitulo.getText().toString();
+                time = eHora.getText().toString();
+                fecha= eFecha.getText().toString();
+                direccion= txtDireccion.getText().toString().trim();
+                descripcion= txtDescripcion.getText().toString().trim();
 
                 if (!txtTitulo.getText().toString().equals("")
                         && !eFecha.getText().toString().equals("")&& !eHora.getText().toString().equals("")) {
-
+                    ejecutarServicio("https://bdconandroidstudio.000webhostapp.com/registrar.php");//llamamos a ka BD remota
                     DbContactos dbContactos = new DbContactos(NuevoActivity.this);
-                    long id = dbContactos.insertarContacto(txtTitulo.getText().toString(), eHora.getText().toString(),eFecha.getText().toString(),txtDireccion.getText().toString(), txtDescripcion.getText().toString());
+                    long id = dbContactos.insertarContacto(txtTitulo.getText().toString(), eHora.getText().toString(),eFecha.getText().toString(),direccion,descripcion);
 
                     if (id > 0) {
+
                         Toast.makeText(NuevoActivity.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
                         limpiar();
                         Intent intent = new Intent(actividad, MainActivity.class);
                         startActivity(intent);
+
                     } else {
                         Toast.makeText(NuevoActivity.this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show();
                     }
@@ -129,4 +151,38 @@ public class NuevoActivity extends AppCompatActivity {
             timePickerDialog.show();
         }
     }
+
+    public void ejecutarServicio(String URL){
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String,String> parametros=new HashMap<String,String>();
+
+                parametros.put("titulo",titulo);
+                parametros.put("hora",time);
+                parametros.put("fecha",fecha);
+                parametros.put("direccion",direccion);
+                parametros.put("descripcion",descripcion);
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+
 }
