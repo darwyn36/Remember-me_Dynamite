930bc0ed7726003ba86f42879d74edaf9d688bd1
Cristhian Real <cristhianreal.p@gmail.com>
2022-04-28 18:52:40 -0400

    Se agrego la opcion de escoger rol en el inicio de sesion

--- app/src/main/java/com/cdp/agenda/LoginActivity.java
+++ app/src/main/java/com/cdp/agenda/LoginActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     TextInputLayout user,contrasenia;
+    Spinner spiRol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         user=(TextInputLayout) findViewById(R.id.usuario);
         contrasenia=(TextInputLayout) findViewById(R.id.contrasenia);
+        spiRol = findViewById(R.id.spirol);
     }
     public void ir(View view){
         Intent intent= new Intent(LoginActivity.this,MainActivity.class);
         startActivity(intent);
+    }
+    public void ir2(View view){
+        String r = String.valueOf(spiRol.getSelectedItem());
+        Toast.makeText(LoginActivity.this, ""+r, Toast.LENGTH_SHORT).show();
     }
 }
