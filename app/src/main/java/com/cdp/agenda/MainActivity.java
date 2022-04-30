d0d0260a54803e82267b46a25694e5b076cffef4
jhonas321 <tayguerjc@gmail.com>
2022-04-30 00:07:50 -0400

    creacion de spinner

--- app/src/main/java/com/cdp/agenda/MainActivity.java
+++ app/src/main/java/com/cdp/agenda/MainActivity.java
@@ -1,63 +1,103 @@
 package com.cdp.agenda;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.cdp.agenda.adaptadores.ListaContactosAdapter;
 import com.cdp.agenda.db.DbContactos;
-import com.cdp.agenda.db.DbHelper;
 import com.cdp.agenda.entidades.Contactos;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
+    private Spinner spinner;
+    private Button boton;
+
     SearchView txtBuscar;
-    RecyclerView listaContactos;
+    //RecyclerView listaContactos;
     ArrayList<Contactos> listaArrayContactos;
     FloatingActionButton fabNuevo;
-    ListaContactosAdapter adapter;
+    //ListaContactosAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.programar);
+
+        //inicializar componentes
+        spinner = findViewById(R.id.spinner);
+        boton = findViewById(R.id.button2);
+
+        //Acciones del boton
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llenarSpinner();
+            }
+        });
+    }
+
+    private void llenarSpinner() {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1,"jose","Garcia","lopez"));
+        usuarios.add(new Usuario(2,"luis","Pacheco","Hernandez"));
+        usuarios.add(new Usuario(3,"macario","Choque","Mento"));
+        usuarios.add(new Usuario(4,"maria","Alvarado","Veliz"));
+
+        ArrayAdapter<Usuario> adapter1 = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,usuarios);
+        spinner.setAdapter(adapter1);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"Realice una seleccion", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         txtBuscar = findViewById(R.id.txtBuscar);
-        listaContactos = findViewById(R.id.listaContactos);
+        //listaContactos = findViewById(R.id.listaContactos);
         fabNuevo = findViewById(R.id.favNuevo);
-        listaContactos.setLayoutManager(new LinearLayoutManager(this));
+        //listaContactos.setLayoutManager(new LinearLayoutManager(this));
 
         DbContactos dbContactos = new DbContactos(MainActivity.this);
 
         listaArrayContactos = new ArrayList<>();
 
-        adapter = new ListaContactosAdapter(dbContactos.mostrarContactos());
-        listaContactos.setAdapter(adapter);
+        //adapter = new ListaContactosAdapter(dbContactos.mostrarContactos());
+        //listaContactos.setAdapter(adapter);
 
-        fabNuevo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nuevoRegistro();
-            }
-        });
-        txtBuscar.setOnQueryTextListener(this);
+        //fabNuevo.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        nuevoRegistro();
+        //    }
+        //});
+        //txtBuscar.setOnQueryTextListener(this);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -89,7 +129,10 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     @Override
     public boolean onQueryTextChange(String s) {
-        adapter.filtrado(s);
+        //adapter.filtrado(s);
         return false;
     }
+
+
+
 }
---
+++ app/src/main/java/com/cdp/agenda/Usuario.java
@@ -0,0 +1,62 @@
+package com.cdp.agenda;
+
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String usuarioCustom;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Usuario(Integer id, String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    @Override
+    public String toString() {
+        this.usuarioCustom = this.usuarioCustom = id+""+nombre+""+apellidoPaterno+""+apellidoMaterno;
+        return usuarioCustom;
+    }
+}
